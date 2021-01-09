d46c82f03cf9367c47a3cd3af62373d664c7032c
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-01-09 19:51:42 +0900

    Initial commit

---
+++ src/main/java/com/example/try_spring_security/TrySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.try_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrySpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrySpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/try_spring_security/TrySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.try_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrySpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
